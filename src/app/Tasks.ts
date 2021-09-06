bfe3259829d0d867d2bc3171d4f0f9bc06fec0bc
PunsisiPerera <punsisi.subhani@gmail.com>
2021-09-06 21:12:38 +0530

    initial components

---
+++ src/app/Tasks.ts
@@ -0,0 +1,11 @@
+export interface Task{
+        cus_id:string,
+        name: string,
+        email:string,
+        phoneNo: number,
+        type: string,
+        dob:string,
+        gender:string,
+        address:string
+
+}
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,54 @@
+import { Task } from "./Tasks"
+
+export const TASKS: Task[] = [
+    {
+        cus_id:"C1",
+        name: "Kamal Perera",
+        email:"kamal@gmail.com",
+        phoneNo: 714566585,
+        type: "Category 1",
+        dob:"12/7/89",
+        gender:"Male",
+        address:"Galle Rd, Panadura"
+    },
+    {
+        cus_id:"C2",
+        name: "Sunil Perera",
+        email:"sunil@gmail.com",
+        phoneNo: 714566585,
+        type: "Category 2",
+        dob:"12/7/89",
+        gender:"Male",
+        address:"Galle Rd, Ambalngoda"
+    },
+    {
+        cus_id:"C3",
+        name: "Nimal Perera",
+        email:"nimal@gmail.com",
+        phoneNo: 714566585,
+        type: "Category 1",
+        dob:"12/7/89",
+        gender:"Male",
+        address:"Galle Rd, Panadura"
+    },
+    {
+        cus_id:"C4",
+        name: "Amali Perera",
+        email:"amali@gmail.com",
+        phoneNo: 714566585,
+        type: "Category 1",
+        dob:"12/7/89",
+        gender:"Female",
+        address:"Galle Rd, Moratuwa"
+    },
+    {
+        cus_id:"C5",
+        name: "Anil Perera",
+        email:"anil@gmail.com",
+        phoneNo: 714566585,
+        type: "Category 1",
+        dob:"12/7/89",
+        gender:"Male",
+        address:"Galle Rd, Kalutara"
+    }
+]
